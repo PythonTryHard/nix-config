d43c31c6e3b0fcb598591240010a332c08626b70
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2024-06-20 17:05:34 +0700

    Add GNOME as a fallback DE

--- nixos/configs/desktop-environment.nix
+++ nixos/configs/desktop-environment.nix
@@ -5,6 +5,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # GNOME for now
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Plasma 6 :D
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
